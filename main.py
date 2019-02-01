96eab3d32c9e5504d9c73257fef4c8f5c4e05ecc
pgriffith <philip.griffith@gmail.com>
2019-02-01 13:51:59 -0500

    Chemin de Fer game finished.

--- main.py
+++ main.py
@@ -23,48 +23,59 @@ class Chemin():
 
     def play(self):
 
-        punter_hits = False
-
         hand = self._deal()
         banker_deal = hand[:3]
         punter_deal = hand[3:]
+        punter_card = punter_deal[-1]
 
         banker1 = int(str(banker_deal[:2].sum())[-1])
         punter1 = int(str(punter_deal[:2].sum())[-1])
 
         banker2 = int(str(banker1 + banker_deal[-1])[-1])
-        punter2 = int(str(punter1 + punter_deal[-1])[-1])
+        punter2 = int(str(punter1 + punter_card)[-1])
 
         print('Banker:', banker_deal)
         print('Punter:', punter_deal)
 
-        print('Initial Banker hand:', banker1)
-        print('Initial Punter hand:', punter1)
-
-        print('Second Banker hand:', banker2)
-        print('Second Punter hand:', punter2)
-
-        result = 'Banker: {} vs Punter: {}\n{{}} wins'.format(banker1,
-                                                              punter1)
-
         if banker1 > 7 or punter1 > 7:
-            if banker1 > punter1:
-                print(result.format('Banker'))
-            elif banker1 < punter1:
-                print(result.format('Punter'))
-            else:
-                print(result.format('Nobody'))
+            self.result(banker1, punter1)
         else:
-            if punter1 < 6:
-                # Player hits
+            if punter1 < 5 or (punter1 == 5 and npr.randint(0, 2)):
+                print('Player hits')
                 punter1 = punter2
-                if banker1
-                    pass
-            else:
-                # Player stands
-                if banker1 < 6:
+                if banker1 < 3:
                     banker1 = banker2
+                    print('Banker hits')
+                elif banker1 == 3 and (punter_card < 8 or (punter_card == 9 and npr.randint(0, 2))):
+                    banker1 = banker2
+                    print('Banker hits')
+                elif banker1 == 4 and (1 < punter_card < 8):
+                    banker1 = banker2
+                    print('Banker hits')
+                elif banker1 == 5 and ((4 < punter_card < 8) or (punter_card == 4 and npr.randint(0, 2))):
+                    banker1 = banker2
+                    print('Banker hits')
+                elif banker1 == 6 and (5 < punter_card < 8):
+                    banker1 = banker2
+                    print('Banker hits')
+            else:
+                print('Player stands')
+                if banker1 < 6 or (banker1 == 6 and npr.randint(0, 2)):
+                    banker1 = banker2
+                    print('Banker hits')
 
+            self.result(banker1, punter1)
+
+    @staticmethod
+    def result(banker, punter):
+
+        result = 'Banker: {} vs Punter: {}\n{{}} wins'.format(banker, punter)
+        if banker > punter:
+            print(result.format('Banker'))
+        elif banker < punter:
+            print(result.format('Punter'))
+        else:
+            print(result.format('Nobody'))
 
 
 c = Chemin()
