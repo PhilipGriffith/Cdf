353ab1546a41dec0cf3c90889989f6ba519f1db8
pgriffith <philip.griffith@gmail.com>
2019-02-01 15:23:09 -0500

    Refactored.

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import numpy.random as npr
 
-class Chemin():
+
+class Chemin:
 
     def __init__(self, num_decks=6):
         
         self.num_decks = num_decks
         self.shoe = []
-        
-        self._build_shoe()
 
-        
+        self._build_shoe()
+        self.banker = 0
+        self.punter = 0
 
     def _build_shoe(self):
 
@@ -34,49 +35,48 @@ class Chemin():
         banker2 = int(str(banker1 + banker_deal[-1])[-1])
         punter2 = int(str(punter1 + punter_card)[-1])
 
-        print('Banker:', banker_deal)
-        print('Punter:', punter_deal)
-
         if banker1 > 7 or punter1 > 7:
             self.result(banker1, punter1)
         else:
             if punter1 < 5 or (punter1 == 5 and npr.randint(0, 2)):
-                print('Player hits')
                 punter1 = punter2
                 if banker1 < 3:
                     banker1 = banker2
-                    print('Banker hits')
                 elif banker1 == 3 and (punter_card < 8 or (punter_card == 9 and npr.randint(0, 2))):
                     banker1 = banker2
-                    print('Banker hits')
                 elif banker1 == 4 and (1 < punter_card < 8):
                     banker1 = banker2
-                    print('Banker hits')
                 elif banker1 == 5 and ((4 < punter_card < 8) or (punter_card == 4 and npr.randint(0, 2))):
                     banker1 = banker2
-                    print('Banker hits')
                 elif banker1 == 6 and (5 < punter_card < 8):
                     banker1 = banker2
-                    print('Banker hits')
             else:
-                print('Player stands')
                 if banker1 < 6 or (banker1 == 6 and npr.randint(0, 2)):
                     banker1 = banker2
-                    print('Banker hits')
 
             self.result(banker1, punter1)
 
-    @staticmethod
-    def result(banker, punter):
+    def result(self, banker, punter, outcome=False):
 
         result = 'Banker: {} vs Punter: {}\n{{}} wins'.format(banker, punter)
         if banker > punter:
-            print(result.format('Banker'))
+            self.banker += 1
+            if outcome:
+                print(result.format('Banker'))
         elif banker < punter:
-            print(result.format('Punter'))
+            self.punter += 1
+            if outcome:
+                print(result.format('Punter'))
         else:
-            print(result.format('Nobody'))
+            if outcome:
+                print(result.format('Nobody'))
 
 
 c = Chemin()
-c.play()
+
+for i in range(100000):
+    c.play()
+
+print(c.banker)
+print(c.punter)
+print(c.banker / c.punter)
